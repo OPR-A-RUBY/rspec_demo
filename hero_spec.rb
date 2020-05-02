97af4a0eb80395596d26909c294502cefe682594
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-02 19:56:32 +0400

    Add power_down and hero_info tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -4,17 +4,21 @@ describe Hero do
   
   it "hero a capitalize name" do 
     hero = Hero.new 'mike'
-    expect(hero.name).to eq 'Mike' # hero.name == 'Mike'
+    expect(hero.name).to eq 'Mike'      # hero.name == 'Mike'
   end
 
-  it "canh power up" do
+  it "can power up" do
     hero = Hero.new 'mike'
-
-    # a = hero.power_up
-    # expect(hero.halth).to eq 110
-
-    # или
     expect(hero.power_up).to eq 110
   end
 
+  it "can power down" do
+    hero = Hero.new 'mike'
+    expect(hero.power_down).to eq 90
+  end
+
+  it "can hero info" do
+    hero = Hero.new 'mike'
+    expect(hero.hero_info).to eq "Mike has a halth of 100"
+  end  
 end
