3565745653564add6130f1843f41350557099937
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-02 16:55:25 +0400

    Add hero entity

--- hero.rb
+++ hero.rb
@@ -1,2 +1,28 @@
 class Hero
+  def initialize(name, halth=100) # Это конструктор (имя, здоровье)
+    @name = name.capitalize
+    @halth = halth
+  end 
+
+  def name
+    @name
+  end
+
+  def power_up
+    @halth += 10
+  end
+
+  def power_down
+    @halth -= 10
+  end
+
+  def hero_info
+    "#{@name} has a halth of #{@halth}"
+  end  
 end
+
+hero = Hero.new 'Mike'
+puts hero.name
+puts hero.power_up
+puts hero.power_down
+puts hero.hero_info
